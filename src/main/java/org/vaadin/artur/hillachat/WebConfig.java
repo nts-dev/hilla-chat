74f0ecb10c4680f4ab3efa531068cd8298a6e466
Daniel Obilo <daniel@nts.nl>
2024-04-16 14:40:56 +0300

    first commit

---
+++ src/main/java/org/vaadin/artur/hillachat/WebConfig.java
@@ -0,0 +1,18 @@
+package org.vaadin.artur.hillachat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3001","http://localhost:3000","http://intranet.nts.nl:3000","http://localhost:40002")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
